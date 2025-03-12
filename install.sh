d784c1dac9afe95de72abcc857398520527f419d
Gurupra5ad <guruprasad2511@gmail.com>
2025-03-12 11:24:01 +0530

    working Alfred URDF

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 sudo apt install ros-jazzy-xacro -y
 sudo apt install ros-jazzy-joint-state-publisher -y
 sudo apt install ros-jazzy-joint-state-publisher-gui -y
+sudo apt install ros-jazzy-ros-gz -y
+sudo apt install ros-jazzy-twist-mux -y
+sudo apt install ros-jazzy-twist-stamper -y
+sudo apt install ros-jazzy-ros2-control -y
+sudo apt install ros-jazzy-ros2-controllers -y
+
